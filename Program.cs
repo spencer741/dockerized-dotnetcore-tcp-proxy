a36426c6108c1a8bd044a411597c88a2d844872b
spencer741 <sa.variable@gmail.com>
2019-12-01 16:06:29 -0600

    Implementeed documentation and rest of target communications code. Refactored big chunks and fixed major bugs.

--- Program.cs
+++ Program.cs
@@ -1,35 +1,81 @@
-﻿using Microsoft.Extensions.Logging;
+﻿//This program is a higher level, barebones implementation of a TCP Proxy server, using HTTP only.
+//HTTPs implementation is not hard to implememt... although not in this code
+
+//You can wrap the NetworkStream provided by TcpClient with an SslStream.This will provide the necessary handling of SSL and certificates.
+//You may have to insert some handler code to perform a certificate validation callback, e.g.through ServicePointManager.ServerCertificateValidationCallback.
+//https://docs.microsoft.com/en-us/dotnet/api/system.net.security.sslstream?redirectedfrom=MSDN&view=netframework-4.8
+
+//I have wrapped this .net core program in a linux docker container and deployed to an Azure Container Instance for cloud hosting...
+//Here is a basic schematic on how to do all of the latter ... (This is very general and doesn't include some things like docker setup).
+//But make sure docker is creating linux containers ... I chose linux to see how everything would work cross platform, and the fact that 
+//Windows containers are a lot bulkier (in addition to the fact that they only support 1709 ... of course I was running 1903, so Windows
+//containers would be more of a hassle in my circumstance).
+
+// Open powershell to a directory of your choice...
+// mkdir WINCONTAINER
+// New-Item Dockerfile
+// dotnet new console -o app -n myapp
+
+//You need to populate the docker file and the program cs (different directories).
+
+/* Dockerfile:
+ * FROM mcr.microsoft.com/dotnet/core/runtime:3.0
+ * COPY app/app/bin/Release/netcoreapp3.0/publish/ app/
+ * ENTRYPOINT ["dotnet", "app/myapp.dll"]
+ */
+
+/* Program.cs
+ * Paste TCP client code in here.
+ */
+
+//Rest of powershell commands...
+//docker login
+//docker build -t myimage -f Dockerfile .
+//dotnet publish -c Release
+// make sure publish directory exists ... somewhere around dir .\bin\release\netcoreapp3.0\
+//docker tag myimage spencer741/myimage:latest
+//docker push spencer741/myimage:latest
+
+//Created by Spencer Arnold
+
+//Some notes on the program itself
+//This implementation could definitely be made more robust at handling TCP connections, especially on the memory handling side of things.
+//However, this is a basic example that meets the requrements of getting my hands wet with C#, .netcore, docker, and Azure.
+
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
 using System.Threading;
 
 namespace NETCORE_TCP_PROXY
 {
     class Program
     {
-
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Started...");
             //Create variable of type TcpListener, nulled out.
             TcpListener server = null;
 
             //Create variables for port and IP Address.
+            //The address index will be different depending on network settings and machine.
+            String strHostName = string.Empty;
+            strHostName = Dns.GetHostName();
+            IPHostEntry ipHostEntry = Dns.GetHostEntry(strHostName);
+            IPAddress[] address = ipHostEntry.AddressList;
+            Console.WriteLine("The Local IP Address: " + address[3].ToString());
             Int32 port = 13000;
-            IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+            IPAddress Addr = IPAddress.Parse(address[3].ToString());
 
             //Create new TcpListener Object and set the appropriate IP and port to listen on
             //Done outside of try catch to terminate listener if catasrophe strikes.
-            server = new TcpListener(localAddr, port);
+            server = new TcpListener(Addr, port);
+
+            Console.WriteLine(server.LocalEndpoint);
 
             try
             {
- 
+
                 #region ***The daemon listens for TCP connections on a specified port number.***
 
                 // Start listening for client requests.
@@ -84,14 +130,14 @@ namespace NETCORE_TCP_PROXY
                         //Parameterized Thread entry point methods can only take type 'object' so internal casting has to be done.
                         thread.Start(NewClient);
 
-                       
+
                         #endregion
 
                     }
 
                     //Once thread has been spawned, the daemon process resumes listening for additional TCP connections from other clients.
                     //Continuing loop here.
-                    
+
                 }
 
                 #endregion
@@ -112,89 +158,144 @@ namespace NETCORE_TCP_PROXY
 
         }
 
-        /*
-         * TODO: This is the last buildout that needs to happen... 
-         * 
-            The thread establishes a new TCP connection to the targeted server. 
-            (Extracting Hostname from beginnging of HTTP message, resolving to IP address)
-
-            The thread falls into a loop in which it acts as an intermediator exchanging 
-            data(reading / writing <or> writing / reading) between the client and the targeted server.
-             
-             
-        */
+       
         private static void HandleTcpConnection(object NewClient)
         {
             //This will be executed on another thread
-
-            TcpClient Client = (TcpClient) NewClient;
-
             Console.WriteLine("Connected! Spawned Thread {0}", Thread.CurrentThread.ManagedThreadId);
 
-            //Buffer variables for reading data
-            byte[] bytes = new byte[1000];
-            string data = null;
+            //Set received tcp client for this thread.
+            TcpClient Client = (TcpClient)NewClient;
 
+            //Get Client stream object for reading and writing
+            NetworkStream ClientStream = Client.GetStream();
 
-            // Get a stream object for reading and writing
-            NetworkStream stream = Client.GetStream();
+            //define target tcpclient and target stream (will get initialized once first read happens)
+            TcpClient Target = null;
+            NetworkStream TargetStream = null;
 
-            int i;
+            //Client buffer variables for reading data
+            byte[] bytesfromclient = new byte[8049];
+            string datafromclient = String.Empty;
 
-            // Loop to receive all the data sent by the client.
-            while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+            //Target buffer variables for reading data
+            byte[] bytesfromtarget = new byte[8049];
+            string datafromtarget = String.Empty;
+
+            //Store byte counts received for Client and Target
+            int bytescli, bytestar;
+
+            //Hostname
+            string hostname;
+
+            while (Client.Connected)
             {
-                // Translate data bytes to a ASCII string.
-                data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
-
-                //Get Hostname from incoming request
-                if (data.Contains("CONNECT") || data.Contains("GET"))
+                try
                 {
-                    string hostname = null;
-                    int hostindex = data.IndexOf("Host: ") + 6;
-                    char c = data[hostindex];
-                    int n = 1;
+                    if (ClientStream.DataAvailable || TargetStream == null)
+                    {
 
-                    while (c != '\n')
-                    { 
-                        hostname += c;
-                        c = data[hostindex + n];
-                        n++;
+                        bytescli = ClientStream.Read(bytesfromclient, 0, bytesfromclient.Length);
+
+                        // Translate data bytes to a ASCII string.
+                        datafromclient = System.Text.Encoding.ASCII.GetString(bytesfromclient, 0, bytesfromclient.Length);
+                        Console.WriteLine("Received data from Client {0}", datafromclient);
+                        //Get Hostname from incoming request
+                        if (datafromclient.Contains("CONNECT") || datafromclient.Contains("GET") || datafromclient.Contains("POST"))
+                        {
+                            //parses out hostname from request.
+                            hostname = GetHostName(datafromclient);
+                            
+                            //Trim hostname to insure parsing integrity.
+                            hostname = hostname.Trim();
+
+                            //establish new tcp client. 
+                            //TcpClient resolves hostname internally, 
+                            //port 80 is default for http on tcp
+                            Target = new TcpClient(hostname, 80);
+                            TargetStream = Target.GetStream();
+
+                            //Write bytes from Client to Target Stream
+                            TargetStream.Write(bytesfromclient, 0, bytescli);
+
+                            //Read bytes from Target
+                            bytestar = TargetStream.Read(bytesfromtarget, 0, bytesfromtarget.Length);
+
+                            //datafromtarget = System.Text.Encoding.ASCII.GetString(bytesfromtarget, 0, bytestar);
+
+                            //Write bytes from target to client.
+                            ClientStream.Write(bytesfromtarget, 0, bytestar);
+                        }
+
                     }
+                    else if (TargetStream.DataAvailable)
+                    {
 
-                    //Console.WriteLine("Hostname: " + host);
-                    //Console.WriteLine("Received: {0}", data);
+                        //Use bytestar count to only send back necessary information.
+                        bytestar = TargetStream.Read(bytesfromtarget, 0, bytesfromtarget.Length);
 
-                    //Resolve Hostname
-                    IPHostEntry hostip = Dns.GetHostEntry(hostname);
+                        datafromtarget = System.Text.Encoding.ASCII.GetString(bytesfromtarget, 0, bytestar);
+                        //Console.WriteLine(datafromtarget);
 
-                    //establish new tcp client
-
+                        ClientStream.Write(bytesfromtarget, 0, bytestar);
+                    }
+                    
                 }
-
-                
+                catch (Exception e)
+                {
+                    Console.WriteLine("ERROR: " + e + "\n\n" + "Continuing . . .");
+                    continue;
+                }
             }
 
+            Console.WriteLine("Cannot read! Closing all Connections.");
 
+            //Force close streams;
+            ClientStream.Close();
+            TargetStream.Close();
 
-
-            //Just for fun. You don't believe it til you see it. GET' ' Really does send a space. 
-            //for (int h = 0; h < bytes.Length; h++)
-            //Console.WriteLine("Byte " + h + " : " + Convert.ToString(bytes[h], 2) + " Char : " + bytes[h].ToString() + '\n');
-
-
-
-            // Process the data sent by the client.
-            //data = data.ToUpper();
-
-            //byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
-
-            // Send back a response.
-            //stream.Write(msg, 0, msg.Length);
-            //Console.WriteLine("Sent: {0}", data);
-
-            // Shutdown and end connection
             Client.Close();
-        }  
+            Target.Close();
+        }
+
+        //This function parses out and returns hostname from a request string
+        private static string GetHostName(string datafromclient)
+        {
+            string hostname = null;
+            int hostindex = datafromclient.IndexOf("Host: ") + 6;
+            char c = datafromclient[hostindex];
+            int n = 1;
+
+            while (c != '\n')
+            {
+                // Console.WriteLine('!' + hostname + '!');
+                hostname += c;
+                c = datafromclient[hostindex + n];
+                n++;
+            }
+
+            return hostname;
+
+        }
+
+
+        //Deprecated ... no longer used.
+        //This function parses out and returns content length from a request string.
+        private static int GetContentLength(string datafromclient)
+        {
+            string S_ContentLength = null;
+            int ContentLengthIndex = datafromclient.IndexOf("Content-Length: ") + 16;
+            char c = datafromclient[ContentLengthIndex];
+            int n = 1;
+
+            while (c != '\n')
+            {
+                S_ContentLength += c;
+                c = datafromclient[ContentLengthIndex + n];
+                n++;
+            }
+
+            return int.Parse(S_ContentLength);
+        }
     }
 }
